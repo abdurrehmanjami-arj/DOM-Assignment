7d960e7629a76c90a32747eba1a224602d9c498b
abdurrehmanjami-arj <abdurrehmanjamiwork@gmail.com>
2025-07-29 17:25:09 +0500

    assignment complete

--- app.js
+++ app.js
@@ -83,7 +83,6 @@
 // const changeFontSize = document.querySelector("#chnage-font-size")
 // changeFontSize.style.fontSize = "24px"
 
-
 // Question No 12. On button click, get a name from input "fullName" and set the text "Hello,
 // [name]" inside a div "greeting".
 
@@ -92,4 +91,87 @@
 
 // function showFullName (){
 //   greeting.innerHTML = "Hello, " + fullName.value
+// }
+
+// Question No 13. Select an image with id "profilePic" and change its src to another image URL.
+
+// const profilePic = document.querySelector("#profilePic");
+// function changePicture() {
+//   profilePic.src =
+//     "https://static.desygner.com/wp-content/uploads/sites/13/2022/05/04141642/Free-Stock-Photos-01.jpg";
+//   profilePic.style.width = "300px";
+//   profilePic.style.height = "300px";
+// }
+
+//Question No 14. Change the text color of all elements with class "highlight" to orange. (Hint: You’ll
+// need querySelectorAll for multiple)
+
+// let changeParaColor = document.querySelectorAll(".highlight");
+// for (let i = 0; i < changeParaColor.length; i++) {
+//   changeParaColor[i].style.color = "orange";
+// }
+
+// Question No 15. Select a checkbox with id "agree" and check whether it is checked or not, then display
+// "Agreed" or "Not Agreed" in a paragraph.
+
+// const checkbox = document.getElementById("agree");
+// const statusText = document.getElementById("statusText");
+
+// function checkAgree() {
+//   if (checkbox.checked) {
+//     statusText.innerHTML = "Agreed";
+//   } else {
+//     statusText.innerHTML = "Not Agreed";
+//   }
+// }
+
+// Question No 16. Get the selected value from a dropdown (select element) with id "options" and display
+// it in "selectedValue" span.
+
+// const selectOption = document.querySelector("#option");
+// const showOption = document.querySelector("#selectedValue");
+// function showSelectedValue() {
+//   showOption.innerHTML = selectOption.value;
+// }
+
+// Question No 17. When a button is clicked, show the current date and time inside a paragraph with id
+// "dateTime".
+
+// const dateTime = document.querySelector("#dateTime");
+
+// function getDateTime() {
+//   const date = new Date();
+//   dateTime.innerHTML = date.toDateString();
+// }
+
+// Question No 18. Select a paragraph with id "togglePara" and hide it (set display: none).
+
+// const togglePara = document.querySelector("#togglePara");
+// function hidePara() {
+//   if (togglePara.style.display === "none") {
+//     togglePara.style.display = "block";
+//   } else {
+//     togglePara.style.display = "none";
+//   }
+// }
+
+// Question No 19. Get a user’s age from an input with id "age" and display "Eligible" if age >= 18,
+// else "Not Eligible" in a span.
+
+// const age = document.querySelector("#age")
+// const showMessage = document.querySelector("#showMessage")
+
+// function displayAge (){
+//   if(age.value >= 18){
+//     showMessage.innerHTML = "Eligible"
+//   } else {
+//     showMessage.innerHTML = "Not Eligible"
+//   }
+// }
+
+// Question No 20. When a button is clicked, clear the value of an input with id "searchBox".
+
+// const searchBox = document.querySelector("#searchBox");
+// function hideInput() {
+//   searchBox.value = "";
 // }
