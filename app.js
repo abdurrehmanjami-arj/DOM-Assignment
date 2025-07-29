b6ee6434a9549cdcdd8c5fb2fe2e414184c8dbe4
abdurrehmanjami-arj <abdurrehmanjamiwork@gmail.com>
2025-07-29 14:23:08 +0500

    12 questions completed

---
+++ app.js
@@ -0,0 +1,95 @@
+// Question No 1. Select a paragraph with id "message" and change its text to "Welcome!".
+
+// const para = document.querySelector("#para");
+// para.innerHTML = "Welcome!"
+
+// Question No 2. Get the value from an input with id "nameInput" and display it inside a div with id
+// "result".
+
+// const nameInput = document.querySelector("#nameInput");
+// const result = document.querySelector("#result");
+// function showResult() {
+//   result.innerHTML = nameInput.value;
+// }
+
+// Question No 3. When a button is clicked, change the background color of a section with id
+// "container" to light blue.
+
+// const container = document.querySelector("#container");
+
+// function changeColor() {
+//   container.style.backgroundColor = "lightblue";
+// }
+
+// Question No 4. Select a heading (h1) with id "mainTitle" and change its color to red.
+
+// const mainTitle = document.querySelector("#mainTitle")
+// mainTitle.style.color = "red"
+
+// Question No 5. Get the number from an input with id "num1" and show it inside a paragraph with id
+// "output".
+
+// const input = document.querySelector("#num1");
+// const output = document.querySelector("#output");
+
+// function generate() {
+//   output.innerHTML = input.value
+// }
+
+// Question No 6. Select a button with class "submit-btn" and log "Button Clicked" to the console
+// when it's clicked.
+
+// const submitBtn = document.querySelector(".submit-btn");
+// function showConsole() {
+//   console.log("Welcome to Class");
+// }
+
+// Question No 7. Get values from two inputs with ids "first" and "second", add them (as numbers),
+// and show the result in a span with id "sum".
+
+// const firstNumber = document.querySelector("#first");
+// const secondNumber = document.querySelector("#second");
+// const sumResult = document.querySelector("#sum");
+
+// function sumNumbers() {
+//   let total = +firstNumber.value + +secondNumber.value;
+//   sumResult.innerHTML = total;
+// }
+
+// Question No 8. Change the text of a paragraph with class "description" to "Updated
+// successfully".
+
+// const description = document.querySelector(".description");
+// function changePara() {
+//   description.innerHTML = "Updated successfully";
+// }
+
+// Question No 9. Select a div with id "box" and set its width to 200px and background color to green.
+
+// const setBoxWidth = document.querySelector("#box");
+// setBoxWidth.style.width = "200px";
+// setBoxWidth.style.height = "200px";
+// setBoxWidth.style.backgroundColor = "green";
+
+// Question No 10. Get the email entered in an input with id "email" and print it in the console.
+
+// const email = document.querySelector("#email");
+// function showEmail() {
+//   console.log(email.value);
+// }
+
+// Question No 11. Select a paragraph and update its font size to 24px.
+
+// const changeFontSize = document.querySelector("#chnage-font-size")
+// changeFontSize.style.fontSize = "24px"
+
+
+// Question No 12. On button click, get a name from input "fullName" and set the text "Hello,
+// [name]" inside a div "greeting".
+
+// const fullName = document.querySelector("#full-name")
+// const greeting = document.querySelector("#greeting")
+
+// function showFullName (){
+//   greeting.innerHTML = "Hello, " + fullName.value
+// }
